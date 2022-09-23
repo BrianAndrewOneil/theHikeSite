9b113877508fdfece98830e327f5c2c5323d7599
BrianAndrewOneil <brian.andrew.oneil@gmail.com>
2022-09-22 21:48:10 -0400

    first commit

---
+++ routes/main.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const router = express.Router();
+const authController = require("../controllers/auth");
+const homeController = require("../controllers/home");
+const postsController = require("../controllers/posts");
+const aboutController = require("../controllers/about");
+const { ensureAuth, ensureGuest } = require("../middleware/auth");
+
+//Main Routes - simplified for now
+router.get("/", homeController.getIndex);
+router.get("/profile", ensureAuth, postsController.getProfile);
+//router.get("/feed", ensureAuth, postsController.getFeed); For this app, auth not required to view feed
+router.get("/feed", postsController.getFeed);
+router.get("/about", aboutController.getAbout);
+router.get("/login", authController.getLogin);
+router.post("/login", authController.postLogin);
+router.get("/logout", authController.logout);
+router.get("/signup", authController.getSignup);
+router.post("/signup", authController.postSignup);
+
+module.exports = router;
